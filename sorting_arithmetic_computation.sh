95c34476bd994da63e9120722e1c3fdb9c2f6090
manoj gaikwad <gaikwadm853@gmail.com>
2021-07-24 14:44:00 +0530

    Added: compute3 ( c + a / b )

--- sorting_arithmetic_computation.sh
+++ sorting_arithmetic_computation.sh
@@ -8,8 +8,11 @@ echo "Value of a : $a"
 echo "Value of b : $b"
 echo "Value of c : $c"
 
-compute1=$(( a + b * c ))
+compute1=$(( $a + $b * $c ))
 echo "Result of compute1 : $compute1"
 
-compute2=$(( a * b + c ))
+compute2=$(( $a * $b + $c ))
 echo "Result of compute2 : $compute2"
+
+compute3=$(( $c + $a / $b ))
+echo "Result of compute3 : $compute3"
