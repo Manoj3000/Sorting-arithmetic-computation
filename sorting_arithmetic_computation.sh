96e65b2e233a0ef89f9673d11d483bb36bd328b6
manoj gaikwad <gaikwadm853@gmail.com>
2021-07-24 14:27:15 +0530

    Added: take values for variables

--- sorting_arithmetic_computation.sh
+++ sorting_arithmetic_computation.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Welcome to sorting arithmetic computation program"
+read -p "Enter value for a : " a
+read -p "Enter value for b : " b
+read -p "Enter value for c : " c
+
+echo "Value of a : $a"
+echo "Value of b : $b"
+echo "Value of c : $c"
